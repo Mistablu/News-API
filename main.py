eb0336c14bb05671adf3ed20b1b729c75ccbfeb5
Morgan <m.garricks@lancaster.ac.uk>
2024-10-30 15:53:23 +0000

    added dynamic topic handling, language control, email subject, hyperlinks and limited to 20 articles

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import requests
 import sendemail
 api_key="3c09cc7910db479a926b79432100e6c5"
-
-url = "https://newsapi.org/v2/everything?q=tesla" \
+topic = "tesla"
+url = f"https://newsapi.org/v2/everything?q={topic}" \
       "&from=2024-09-30&sortBy=publishedAt&apiKe" \
-      "y=3c09cc7910db479a926b79432100e6c5"
+      "y=3c09cc7910db479a926b79432100e6c5&language=en"
 
 request = requests.get(url)
 content = request.json()
 message = ""
-for article in content["articles"]:
+for article in content["articles"][:20]:
     if article["title"] is not None and article["description"] is not None:
-        message = message + article["title"] + "\n" + article["description"] + "\n\n"
+        message = message + article["title"] + \
+            "\n" + article["description"] \
+            + "\n" + article["url"] + "\n\n"
 
+message = "Subject: Today's news" + "\n" + message
 message=message.encode("utf-8")
 sendemail.send_email(message)
