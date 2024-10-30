30c802d35e3bed917c12b8eba38e206d2703b4b0
Morgan <m.garricks@lancaster.ac.uk>
2024-10-30 15:35:28 +0000

    added email functionality

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import sendemail
 api_key="3c09cc7910db479a926b79432100e6c5"
 
 url = "https://newsapi.org/v2/everything?q=tesla" \
@@ -7,7 +8,10 @@ url = "https://newsapi.org/v2/everything?q=tesla" \
 
 request = requests.get(url)
 content = request.json()
-
+message = ""
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    if article["title"] is not None and article["description"] is not None:
+        message = message + article["title"] + "\n" + article["description"] + "\n\n"
+
+message=message.encode("utf-8")
+sendemail.send_email(message)
